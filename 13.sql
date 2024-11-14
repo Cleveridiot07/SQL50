9629ebfc3111e5bf91251901b044f0fbdcbae726
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-11-14 07:11:46 +0530

    Created 13.sql

---
+++ 13.sql
@@ -0,0 +1,14 @@
+SELECT name
+FROM Employee
+-- The subquery below finds all manager IDs that have five or more employees
+JOIN 
+(
+    SELECT managerId, COUNT(managerId) 
+    FROM Employee
+    -- Group by managerId to count the number of employees each manager has
+    GROUP BY managerId
+    -- Use HAVING to filter for managers with at least 5 employees
+    HAVING COUNT(managerId) >= 5
+) AS subquery
+-- Join the main Employee table with the subquery on matching manager IDs
+ON Employee.id = subquery.managerId;
